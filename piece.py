62f780072dc8d899df0bb8e19554e45ef79e9cca
Teegee88 <13362@student.ecam.be>
2016-04-14 11:15:24 +0200

    o

--- piece.py
+++ piece.py
@@ -1,10 +1,15 @@
 #classe piece
 class Piece:
-	
+"""La classe piece est composée d'un identifiant pieceID
+D'une liste d'objets capteurs
+D'une liste de consignes"""
+#constructeur	
+def _init_(self, pieceID)
+	self.pieceID = ""
 	capteur1 = Capteur()
 	
-	def fonction1()
-		name = "Teegee"
-		name2 = "David"
-		print "Hello, %s!" %name
-		print "Hello, %s" %name2
+def fonction1()
+	name = "Teegee"
+	name2 = "David"
+	print "Hello, %s!" %name
+	print "Hello, %s" %name2
